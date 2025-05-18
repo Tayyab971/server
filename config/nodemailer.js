872b0fd69e08030b2400f2723d793fd04d31a36d
tayyab971 <tayyabejaz971@gmail.com>
2025-05-19 01:21:33 +0500

    added nodemailer things to env.

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,8 +4,8 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "tayyab.ejaz@mqvantage.com",
-        pass: "txujyskypwytmldv",
+        user: process.env.NODEMAILER_EMAIL,
+        pass: process.env.NODEMAILER_EMAIL_PASSWORD,
     },
 });
 export default transporter;
