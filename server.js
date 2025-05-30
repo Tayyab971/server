1fefaae6d2823b94cc326f1d9980bbd09e8e0071
tayyab971 <tayyabejaz971@gmail.com>
2025-05-31 02:55:37 +0500

    fixed server deployment issue frontend domain added to allowed list.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.port || 4000
 
 connectDB()
-const allowedOrigins = ["http://localhost:5173"]
+const allowedOrigins = ["http://localhost:5173", "https://mern-auth-lyart-one.vercel.app/"]
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: allowedOrigins, credentials: true }))
